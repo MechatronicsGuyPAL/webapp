7a688961f4f9b5ab24376f3216e0ab59ab7c0652
phil <philipalong@hotmail.com>
2017-04-25 11:10:43 +0000

    altered models.py, added new pages for testing and crater evaluation, implemented crater viewing and auto crater loading. Future goal, look into further crater limitations i.e. small pixel sizes omitted and picking recent entries with a minimum timestamp difference

--- app/models.py
+++ app/models.py
@@ -1,29 +1,25 @@
 from app import db
 
 class CDA(db.Model):
-    __tablename__='results'
+    __tablename__='CDA_results'
     id = db.Column(db.Integer, primary_key=True)
     image = db.Column(db.String(120))
     x1 = db.Column(db.SmallInteger)
     x2 = db.Column(db.SmallInteger)
     y1 = db.Column(db.SmallInteger)
     y2 = db.Column(db.SmallInteger)
-    GT_status = db.Column(db.String(2),nullable=True)
+    GT_conflict = db.Column(db.Boolean,default=False)
     score = db.Column(db.Float)
     votes = db.Column(db.SmallInteger,default=0)
-    yes = db.Column(db.Integer,default=0)
-    no = db.Column(db.Integer,default=0)
+    yes = db.Column(db.SmallInteger,default=0)
+    no = db.Column(db.SmallInteger,default=0)
     unsure = db.Column(db.SmallInteger,default=0)
     result = db.Column(db.SmallInteger,default=3)
     timestamp = db.Column(db.DateTime)
-    testentry = db.Column(db.Boolean,default=False)
-    testanswer= db.Column(db.Boolean, default=False)
-
-
-
 
     def __repr__(self):
         return '<CDA %r>' % self.id
+
 
 class GroundTruth(db.Model):
     __tablename__='groundtruths'
@@ -34,8 +30,18 @@ class GroundTruth(db.Model):
     y1 = db.Column(db.SmallInteger)
     y2 = db.Column(db.SmallInteger)
 
-
     def __repr__(self):
         return '<GroundTruth %r>' % self.id
 
 
+class Training(db.Model):
+    __tablename__='training_entries'
+    id = db.Column(db.Integer, primary_key=True)
+    image = db.Column(db.String(120))
+    x1 = db.Column(db.SmallInteger)
+    x2 = db.Column(db.SmallInteger)
+    y1 = db.Column(db.SmallInteger)
+    y2 = db.Column(db.SmallInteger)
+
+    def __repr__(self):
+        return '<Training %r>' % self.id
---
+++ db_populate_CDA.py
@@ -0,0 +1,30 @@
+#!flask/bin/python
+from app import db, models
+import json
+from pprint import pprint
+from datetime import datetime
+
+#Delete old database entries before importing new data
+entries = models.CDA.query.all()
+for entry in entries:
+    db.session.delete(entry)
+
+db.session.commit()
+
+#Open the specified JSON file and import data
+with open('app/static/CDA.json') as data_file:
+        objects = [ i for i in json.load(data_file) ]
+
+for i, val in enumerate(objects):
+        for j, val in enumerate(objects[i]['rects']):
+                record = models.CDA(image=objects[i]['image_path'],
+                x1 = objects[i]['rects'][j]['x1'],
+                x2 = objects[i]['rects'][j]['x2'],
+                y1 = objects[i]['rects'][j]['y1'],
+                y2 = objects[i]['rects'][j]['y2'],
+                score = objects[i]['rects'][j]['score'],
+                timestamp = datetime.utcnow())
+                db.session.add(record)
+
+db.session.commit()
+
